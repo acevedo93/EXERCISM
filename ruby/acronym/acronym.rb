2a3cff1c53748f9022fb50e8fe6faa206268c558
DAVID ACEVEDO <davidacevedo@localDavid.local>
2019-05-05 23:12:46 -0500

    fix acronym

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,19 +1,5 @@
 class Acronym
-
-    def self.abbreviate text
-        acronym = ''
-        text.split(' ').each do |s|
-            if s[0] =~ /[A-Za-z]/
-                into = s.split('-')
-                if into.length > 0
-                    into.each do |ss|
-                        acronym += ss[0].upcase
-                    end    
-                elsif
-                acronym += s[0].upcase
-                end
-            end
-        end
-        acronym
-    end
+	def self.abbreviate text
+		text.scan(/(\A|\W)(\w)/).collect{|s| s[1]}.join.upcase
+	end
 end
