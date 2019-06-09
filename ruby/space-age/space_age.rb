75f32373ae72e2d2a15476f46ab2c1a5eb529625
DAVID ACEVEDO <davidacevedo@localDavid.local>
2019-06-09 15:18:15 -0500

    correction space_age

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -1,48 +1,25 @@
-class SpaceAge 
-  def initialize seconds
-    @seconds_earth = 31557600 #segundos de un año de la tierra
-    @seconds = seconds
-    @seconds_day_earth = 86400 # segundos en un dia 
+class SpaceAge
+    @age
+  ORBITAL_PERIODS = {"earth"=> 1,
+                     'mercury' => 0.2408467,
+                     'venus' => 0.61519726,
+                     'mars' => 1.8808158,
+                     'jupiter' => 11.862615,
+                     'saturn' => 29.447498,
+                     'uranus' => 84.016846,
+                     'neptune' => 164.79132}
+    def initialize(age)
+        @age = age
+    end
+    def method_missing(method)
+      # All of the methods will have the form "on_<planet>"
+      planet_name = method.to_s.split('_')[1]
+      compute_age(planet_name)
+    end
+    private
+   def compute_age(planet)
+     # use the formula above
+     age_in_yrs = @age * (1.0 / 31557600) * (1.0 / ORBITAL_PERIODS[planet])
+     return (age_in_yrs).round(2)
+   end
   end
-
-  def self.on_earth
-    puts (@seconds.to_f/@seconds_earth).round(2)
-  end
-
-  def on_mercury
-    orbital_period_years_earth = 0.2408467 # tiempo años tierra que demora dar la vuelta
-    days_on_year = orbital_period_years_earth * 365 # dias en tierra que da la orbita
-    puts days_on_year.round
-    seconds_on_year = days_on_year * @seconds_day_earth #segungos en tierra que da la orbita
-    puts seconds_on_year
-    final = ((orbital_period_years_earth * @seconds.to_f) / seconds_on_year)
-    puts final
-  end
-  # def self.on_venus
-  #   orbital_period = 0.61519726
-  #   puts (age.to_f/@seconds_earth).round(2)
-  # end
-  # def self.on_mars
-  #   orbital_period = 1.8808158
-  #   puts (age.to_f/@seconds_earth).round(2)
-  # end
-  # def self.on_jupiter
-  #   orbital_period = 11.862615
-  #   puts (age.to_f/@seconds_earth).round(2)
-  # end
-  # def self.on_uranus
-  #   orbital_period = 84.016846
-  #   puts (age.to_f/@seconds_earth).round(2)
-  # end
-  # def self.on_neptune
-  #   orbital_period = 164.79132
-  #   puts (age.to_f/@seconds_earth).round(2)
-  # end
-  # def self.on_earth
-  #   orbital_period = 0.61519726
-  #   puts (age.to_f/@seconds_earth).round(2)
-  # end
-end
-
-calculate_age = SpaceAge.new(2_134_835_688)
-calculate_age.on_mercury
